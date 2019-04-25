4f90b265b8ff19ac3c7145a849580a6402a150a3
Chittipol <chitipol1_005@hotmail.co.th>
2019-04-25 14:10:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'q8PVpY/MIt5Lrh3utBzYwY03r3BAlRb0raFemPhtUzmHCh7YNb40rboiLvfzu2sOdHRL22Jd90NmSDJqYtZwWgnY6NymB1gAhxelTvgLHUf9AB6X0wdJkwuwrb+xao/+WNMlgsqemj2ArBQuLOw/jQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
